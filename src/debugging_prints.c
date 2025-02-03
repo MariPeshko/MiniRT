61d5b49919d42b36d6d1cd1ee9d72a78d60105ed
steffi <steffi.bap@gmail.com>
2025-02-03 08:44:32 +0100

    debugging functions

--- src/debugging_prints.c
+++ src/debugging_prints.c
@@ -1,5 +1,15 @@
+/*
+This file is for Debugging functions only. So all the lovely things
+that help in coding, but should under no circumstances still be in the
+code for an evaluation.
+*/
+
 #include "../inc/miniRT.h"
 
+/*
+takes a two dimensional array of characters and prints it in the format
+Index : [map[index]]\n
+*/
 void    print_map(char **map)
 {
     printf("\nPRINT MAP\n");
