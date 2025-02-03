1ebad587ebd92a124f1ad3f5bbae731e08cb5bf6
steffi <steffi.bap@gmail.com>
2025-02-03 08:43:14 +0100

    added display error function.

--- src/error_handling.c
+++ src/error_handling.c
@@ -1,7 +1,11 @@
 #include "../inc/miniRT.h"
 
+/*
+msg: Macro defined Error message
+prints Error message given as Argument
+*/
 int	display_error(char *msg)
 {
 	printf("\nError\n%s\n", msg);
-	return (1);
+	return (FAILURE);
 }
