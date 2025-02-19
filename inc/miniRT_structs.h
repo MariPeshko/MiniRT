8e3c69fee028604e6dd9acbdf6488564a3547a70
MariPeshko <marunapeshko@gmail.com>
2025-02-19 18:53:29 +0100

    feat. structs for config file

--- inc/miniRT_structs.h
+++ inc/miniRT_structs.h
@@ -42,26 +42,44 @@ typedef struct s_ambient
 	s_color	col;
 }	s_ambient ;
 
+// the light brightness ratio in range [0.0,1.0]
+// (unused in mandatory part)R,G,B colors in range [0-255]
 typedef struct s_light
 {
-	/* data */
+	t_point		point;
+	double		bright;
+	s_color		col;
 }	s_light ;
 
 typedef struct s_planes
 {
+	t_point		point;
+	t_vector	norm_vec;
+	s_color		col;
+	int			id; // identifier
 	struct s_planes	*next;
 }	s_planes ;
 
 typedef struct s_spheres
 {
-	/* data */
+	t_point		point;
+	double		diam;
+	s_color		col;
+	int			id; // identifier
 	struct s_spheres	*next;
 }	s_spheres ;
 
-typedef struct s_cylinders
+// Cylinder(s)
+typedef struct s_cys
 {
-	struct s_cylinders	*next;
-}	s_cylinders ;
+	t_point		point;
+	t_vector	norm_vec;
+	double		diam;
+	double		height;
+	s_color		col;
+	int			id; // identifier
+	struct s_cys	*next;
+}	s_cys ;
 
 // Structure to store all information from a config file .rt
 typedef struct s_config
@@ -71,7 +89,7 @@ typedef struct s_config
 	s_light		light;
 	s_planes	*pl; // linked list
 	s_spheres	*sp; // linked list
-	s_cylinders	*cy; // linked list
+	s_cys		*cy; // linked list
 }	s_config ;
 
 typedef struct mini_rt
